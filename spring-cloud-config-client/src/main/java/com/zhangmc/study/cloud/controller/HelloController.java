73eb0976a3d2408b06377014f413f4a7f332be27
zhangMC <1129414986@qq.com>
2020-07-26 18:26:36 +0800

    测试spring-cloud-config-refresh

--- spring-cloud-config-client/src/main/java/com/zhangmc/study/cloud/controller/HelloController.java
+++ spring-cloud-config-client/src/main/java/com/zhangmc/study/cloud/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.zhangmc.study.cloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -12,6 +13,7 @@ import reactor.core.publisher.Mono;
  * @date 2020/7/26 16:59
  **/
 @RestController
+@RefreshScope
 public class HelloController {
 
     @Value("${info.profile:error}")
